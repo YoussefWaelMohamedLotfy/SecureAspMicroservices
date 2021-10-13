09cab27a81889404e19865192ee792d621d02f8a
Youssef Wael Mohamed-Lotfy Mousa <40206862+YoussefWaelMohamedLotfy@users.noreply.github.com>
2021-10-13 17:46:14 +0200

    Added Movies.Client Microservice

---
+++ Movies.Client/Controllers/MoviesController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Movies.Client.ApiServices;
+using Movies.Client.Models;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Movies.Client.Controllers
+{
+    [Authorize]
+    public class MoviesController : Controller
+    {
+        private readonly IMovieApiService _movieApiService;
+
+        public MoviesController(IMovieApiService movieApiService)
+        {
+            _movieApiService = movieApiService ?? throw new ArgumentNullException(nameof(movieApiService));
+        }
+
+        // GET: Movies
+        public async Task<IActionResult> Index()
+        {
+            await LogTokenAndClaims();
+            return View(await _movieApiService.GetMovies());
+        }
+        public async Task LogTokenAndClaims()
+        {
+            var identityToken = await HttpContext.GetTokenAsync(OpenIdConnectParameterNames.IdToken);
+
+            Debug.WriteLine($"Identity token: {identityToken}");
+
+            foreach (var claim in User.Claims)
+            {
+                Debug.WriteLine($"Claim type: {claim.Type} - Claim value: {claim.Value}");
+            }
+        }
+
+        [Authorize(Roles = "admin")]
+        public async Task<IActionResult> OnlyAdmin()
+        {
+            var userInfo = await _movieApiService.GetUserInfo();
+            return View(userInfo);
+        }
+
+
+        // GET: Movies/Details/5
+        public IActionResult Details(int? id)
+        {
+            return View();
+        }
+
+        // GET: Movies/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movies/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("Id,Title,Genre,ReleaseDate,Owner")] Movie movie)
+        {
+            return View();
+        }
+
+        // GET: Movies/Edit/5
+        public IActionResult Edit(int? id)
+        {
+            return View();
+        }
+
+        // POST: Movies/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int id, [Bind("Id,Title,Genre,ReleaseDate,Owner")] Movie movie)
+        {
+            return View();
+        }
+
+        // GET: Movies/Delete/5
+        public IActionResult Delete(int? id)
+        {
+            return View();
+        }
+
+        // POST: Movies/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            return View();
+        }
+
+        private bool MovieExists(int id)
+        {
+            return true;
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+    }
+}
